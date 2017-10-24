c36954c59f7033e1f126fd792aa2bb19b34c00a5
Trine <trine@dekode.no>
2017-10-24 11:33:03 +0200

    Add form class and template

---
+++ hogan-form.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Plugin Name: Hogan Module: Form
+ * Plugin URI: https://github.com/dekodeinteraktiv/hogan-form
+ * Description: Form Module for Hogan, requires Gravity Forms.
+ * Version: 1.0.0-dev
+ * Author: Dekode
+ * Author URI: https://dekode.no
+ * License: GPL-3.0
+ * License URI: https://www.gnu.org/licenses/gpl-3.0.en.html
+ *
+ * Text Domain: hogan-form
+ * Domain Path: /languages/
+ *
+ * @package Hogan
+ * @author Dekode
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+require_once 'class-form.php';
+
+add_action( 'plugins_loaded', function() {
+	load_plugin_textdomain( 'hogan-form', false, '/languages' );
+} );
+
+add_action( 'hogan/include_modules', function() {
+	hogan_register_module( new \Dekode\Hogan\Form() );
+} );
