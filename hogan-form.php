cc0394ab5703cc7a77e704879ff404ab2296ffc8
pederan <peder1976@gmail.com>
2017-11-22 13:51:20 +0100

    updated version number for first release

--- hogan-form.php
+++ hogan-form.php
@@ -3,7 +3,7 @@
  * Plugin Name: Hogan Module: Form
  * Plugin URI: https://github.com/dekodeinteraktiv/hogan-form
  * Description: Form Module for Hogan, requires Gravity Forms or Contact Form 7.
- * Version: 1.0.0-dev
+ * Version: 1.0.0
  * Author: Dekode
  * Author URI: https://dekode.no
  * License: GPL-3.0
